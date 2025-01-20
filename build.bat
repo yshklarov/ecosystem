6e67581aeaaddc61a1659ede5061ac3cbf3d9fd1
Yakov Shklarov <yakov@shklarov.com>
2025-01-20 01:16:00 -0800

    Cleanup

--- build.bat
+++ build.bat
@@ -1,2 +1,4 @@
+@echo off
+
 cl /std:c17 /Zi /O2 /GL /Qpar .\ecosystem.c /I"./external/inc" /link user32.lib gdi32.lib /OUT:ecosystem.exe
 cl /std:c17 /Zi /O2 /GL /Qpar .\util\util_json_test.c /link /OUT:.\util\util_json_test.exe
